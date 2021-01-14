613b54adc09dd55da329829301745ebd9774edc6
dialuponline <dialuponline@proton.me>
2021-01-14 16:10:52 +0000

    new script

--- source/detect.py
+++ source/detect.py
@@ -2,4 +2,4 @@
 """
 This a modified version of the object detector available from caffe.
 A good starting point thus is to take a look at the original version:
-http://
+http://nbviewer.ipython.org/github/B
