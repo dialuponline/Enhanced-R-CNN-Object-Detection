27cac60983b5dda61f93efb9675f128b14898cf4
dialuponline <dialuponline@proton.me>
2021-01-24 08:14:55 +0000

    Unit test update

--- source/detect.py
+++ source/detect.py
@@ -2,4 +2,5 @@
 """
 This a modified version of the object detector available from caffe.
 A good starting point thus is to take a look at the original version:
-http://nbviewer.ipython.org/github/BVLC/caffe/blob/master/exampl
+http://nbviewer.ipython.org/github/BVLC/caffe/blob/master/examples/detection.ipynb
+The versi
