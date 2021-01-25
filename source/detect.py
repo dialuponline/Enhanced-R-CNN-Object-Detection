88a1b11278d1c27293b90b0b7290d0d65d416ad2
dialuponline <dialuponline@proton.me>
2021-01-25 09:03:37 +0000

    form updates

--- source/detect.py
+++ source/detect.py
@@ -3,4 +3,6 @@
 This a modified version of the object detector available from caffe.
 A good starting point thus is to take a look at the original version:
 http://nbviewer.ipython.org/github/BVLC/caffe/blob/master/examples/detection.ipynb
-The version has been modified by Alessandro Ferrari (alessandroferrari87
+The version has been modified by Alessandro Ferrari (alessandroferrari87@gmail.com).
+
+detect.py is an out-of-the-
