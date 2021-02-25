e43f18675c2c55ef8b386b7a14d4782e77ca7537
dialuponline <dialuponline@proton.me>
2021-02-25 21:25:27 +0000

    Latest changes

--- source/detect.py
+++ source/detect.py
@@ -5,4 +5,4 @@ A good starting point thus is to take a look at the original version:
 http://nbviewer.ipython.org/github/BVLC/caffe/blob/master/examples/detection.ipynb
 The version has been modified by Alessandro Ferrari (alessandroferrari87@gmail.com).
 
-detect.py is an out-of-the-
+detect.py is an out-of-the-box windowed detector callable f
