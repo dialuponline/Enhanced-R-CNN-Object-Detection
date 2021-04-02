be0fb1306bd0a5309e54d90caf9bd27926b33356
dialuponline <dialuponline@proton.me>
2021-04-02 16:36:22 +0000

    update desc

--- source/detect.py
+++ source/detect.py
@@ -18,4 +18,5 @@ Example usage:
 python detect.py --crop_mode=bing 
 --pretrained_model=/path/to/caffe/models/bvlc_reference_rcnn_ilsvrc13/bvlc_reference_rcnn_ilsvrc13.caffemodel 
 --model_def=/path/to/caffe/models/bvlc_reference_rcnn_ilsvrc13/deploy.prototxt 
---mean_file=/path/to/caffe/python/caffe/imagenet/ilsvrc_
+--mean_file=/path/to/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy --gpu 
+--raw_scale=255 --
