84996b8beaacd5d956d8ec079397eada880e81c0
dialuponline <dialuponline@proton.me>
2021-04-02 13:31:25 +0000

    RuboCop: work on LineLength

--- source/detect.py
+++ source/detect.py
@@ -18,4 +18,4 @@ Example usage:
 python detect.py --crop_mode=bing 
 --pretrained_model=/path/to/caffe/models/bvlc_reference_rcnn_ilsvrc13/bvlc_reference_rcnn_ilsvrc13.caffemodel 
 --model_def=/path/to/caffe/models/bvlc_reference_rcnn_ilsvrc13/deploy.prototxt 
---mean_fil
+--mean_file=/path/to/caffe/py
