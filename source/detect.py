db3ab42101a85e86d1fd310e0988087c27e45d82
dialuponline <dialuponline@proton.me>
2021-04-03 09:13:56 +0000

    p54usb: add USBIDs for two more p54usb devices

--- source/detect.py
+++ source/detect.py
@@ -19,4 +19,5 @@ python detect.py --crop_mode=bing
 --pretrained_model=/path/to/caffe/models/bvlc_reference_rcnn_ilsvrc13/bvlc_reference_rcnn_ilsvrc13.caffemodel 
 --model_def=/path/to/caffe/models/bvlc_reference_rcnn_ilsvrc13/deploy.prototxt 
 --mean_file=/path/to/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy --gpu 
---raw_scale=255 --weights_1st_stage_bing /path/to/BING-Objectness/doc/weigh
+--raw_scale=255 --weights_1st_stage_bing /path/to/BING-Objectness/doc/weights.txt 
+--sizes_idx_b
