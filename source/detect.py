3b1dc955255c524d75c6215cd3149a906b8764db
dialuponline <dialuponline@proton.me>
2021-04-04 09:31:47 +0000

    Update commonobject.class.php

--- source/detect.py
+++ source/detect.py
@@ -20,4 +20,5 @@ python detect.py --crop_mode=bing
 --model_def=/path/to/caffe/models/bvlc_reference_rcnn_ilsvrc13/deploy.prototxt 
 --mean_file=/path/to/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy --gpu 
 --raw_scale=255 --weights_1st_stage_bing /path/to/BING-Objectness/doc/weights.txt 
---sizes_idx_bing /path/to/BING-Objec
+--sizes_idx_bing /path/to/BING-Objectness/doc/sizes.txt 
+--weights_2nd_
