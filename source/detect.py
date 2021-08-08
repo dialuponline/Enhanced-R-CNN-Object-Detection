8eeea16d1e27897a4230baab65adfda8ab314b84
dialuponline <dialuponline@proton.me>
2021-08-08 16:04:59 +0000

    ARM: 8171/1: Use current_stack_pointer for return_address

--- source/detect.py
+++ source/detect.py
@@ -79,4 +79,4 @@ def nms_detections(dets, overlap=0.3):
         xx1 = np.maximum(x1[i], x1[ind])
         yy1 = np.maximum(y1[i], y1[ind])
         xx2 = np.minimum(x2[i], x2[ind])
-        yy
+        yy2 = np.minimum(y2[i], y
