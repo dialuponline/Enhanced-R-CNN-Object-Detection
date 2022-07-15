8c9c4688cd691da5893a674dadc8b8cab4806da9
dialuponline <dialuponline@proton.me>
2022-07-15 14:39:32 +0000

    Fixed a comment.

--- source/detect.py
+++ source/detect.py
@@ -220,4 +220,5 @@ def main(argv):
         if not os.path.exists(args.weights_1st_stage_bing_fn):
             print "Error the path specified for weights_1st_stage_bing_fn %s does not exist."%args.weights_1st_stage_bing_fn
             sys.exit(2)
-        weights_1st_stage_bing = np.genfromtxt(args.weights_1st_stage_bing_fn, delimit
+        weights_1st_stage_bing = np.genfromtxt(args.weights_1st_stage_bing_fn, delimiter=',', dtype=np.float32)
+   
