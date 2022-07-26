c6c4b83cae1a4b8a600429652bcce058949a6dc7
dialuponline <dialuponline@proton.me>
2022-07-26 06:28:27 +0000

    Release v1.0.3

--- source/detect.py
+++ source/detect.py
@@ -227,4 +227,5 @@ def main(argv):
     if not args.weights_2nd_stage_bing_fn is None:
         if not os.path.exists(args.weights_2nd_stage_bing_fn):
             print "Error: the path specified for weights_2nd_stage_bing_fn %s does not exist."%args.weights_2nd_stage_bing_fn
-           
+            sys.exit(2)
+        f
