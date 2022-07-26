94a245d24fae1308224e7dfddae0189c3bebccf1
dialuponline <dialuponline@proton.me>
2022-07-26 22:23:37 +0000

    small changes.

--- source/detect.py
+++ source/detect.py
@@ -231,4 +231,5 @@ def main(argv):
         f = open(args.weights_2nd_stage_bing_fn,"r")
         weights_2nd_stage_bing_str = f.read()
         f.close()
-        weights_2nd_stage_bing = json.loads(weights_2nd_st
+        weights_2nd_stage_bing = json.loads(weights_2nd_stage_bing_str)
+    else:
