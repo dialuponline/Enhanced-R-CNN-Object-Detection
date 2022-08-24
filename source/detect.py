b381a472b73bf473cfe52516ba5dbe411e8a8362
dialuponline <dialuponline@proton.me>
2022-08-24 07:07:15 +0000

    Updated csproj

--- source/detect.py
+++ source/detect.py
@@ -252,4 +252,5 @@ def main(argv):
     # Make detector.
     detector = Detector(args.model_def, args.pretrained_model,
             gpu=args.gpu, mean=mean,
-            input_scale=args.input_scale,
+            input_scale=args.input_scale, raw_scale=args.raw_scale,
+     
