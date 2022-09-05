02c33251fcf5375eb07843ba8347132932c3a19b
dialuponline <dialuponline@proton.me>
2022-09-05 14:33:53 +0000

    x86, mm, hugetlb: Add missing TLB page invalidation for hugetlb_cow()

--- source/detect.py
+++ source/detect.py
@@ -257,4 +257,5 @@ def main(argv):
             context_pad=args.context_pad,
             weights_1st_stage_bing = weights_1st_stage_bing, 
             sizes_idx_bing = sizes_idx_bing,
-            weights_2nd_stage_bing = weights_
+            weights_2nd_stage_bing = weights_2nd_stage_bing, 
+        
