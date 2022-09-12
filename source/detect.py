e5070d8ac8484a6568f51203412ae8ae2e203e0b
dialuponline <dialuponline@proton.me>
2022-09-12 10:13:09 +0000

    Bump 0.0.2

--- source/detect.py
+++ source/detect.py
@@ -259,4 +259,7 @@ def main(argv):
             sizes_idx_bing = sizes_idx_bing,
             weights_2nd_stage_bing = weights_2nd_stage_bing, 
             num_bbs_psz_bing = args.num_bbs_psz_bing, 
-            num_bbs_final_bing = args.num_bbs_final_bing
+            num_bbs_final_bing = args.num_bbs_final_bing)
+
+    if args.gpu:
+        
