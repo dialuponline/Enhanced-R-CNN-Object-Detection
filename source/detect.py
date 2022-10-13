488be3016218d66c132ff02266b8d0905bcc8fee
dialuponline <dialuponline@proton.me>
2022-10-13 14:19:10 +0000

    Profile

--- source/detect.py
+++ source/detect.py
@@ -290,4 +290,5 @@ def main(argv):
         mask = indices[1]==cls
         bbs_idx = indices[0][mask]
         bbs_for_nms = [ np.array( (detections[idx]["window"][1], detections[idx]["window"][0], detections[idx]["window"][3], detections[idx]["window"][2], predictions[idx,cls]) )  for idx in bbs_idx]
-        for detection in list(nms_detections(np.array(bbs
+        for detection in list(nms_detections(np.array(bbs_for_nms))):
+            
